55484a93387181e0989d1058aed43c3a1b13e7c4
Douglas White <white.do@northeastern.edu>
2023-05-23 19:39:08 -0400

    fixed some paths and little things

--- 4_Hi-C/run_N_dense_to_hic.bash
+++ 4_Hi-C/run_N_dense_to_hic.bash
@@ -1,6 +1,6 @@
 cd /work/dipierrolab/secret/final_Hi-C_maps/
-for j in 105 106 107
+for j in `seq 192 200`
 do 
-sed "s/segment_index/$j/g" /home/white.do/DiPierroLab_Douglas/Hi-C/submit_dense_to_hic.bash > ./submit_dense_to_hic_$j.bash
+sed "s/segment_index/$j/g" /home/white.do/DiPierroLab_Douglas/4_Hi-C/submit_dense_to_hic.bash > ./submit_dense_to_hic_$j.bash
 sbatch submit_dense_to_hic_$j.bash 
 done
