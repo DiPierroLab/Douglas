cb7932aee0a574ad8674445fb652f3a6e705543c
Douglas White <white.do@northeastern.edu>
2023-05-23 14:49:51 -0400

    worked on the scripts which store trajetories

--- 6_extra_tools/copy_trajectories.bash
+++ 6_extra_tools/copy_trajectories.bash
@@ -1,8 +1,12 @@
 cd /scratch/white.do/Pairing/
-for i in `seq 192 200`
-do
-sed "s/segment_index/$i/g" /home/white.do/DiPierroLab_Douglas/6_extra_tools/copy_trajectories_directory_N.bash > ./copy_trajectories_directory_$i.bash
-sed "s/segment_index/$i/g" /home/white.do/DiPierroLab_Douglas/6_extra_tools/submit_copy_trajectories_directory_N.bash > ./submit_copy_trajectories_directory_$i.bash
-sbatch submit_copy_trajectories_directory_$i.bash
-done
 
+for j in `seq 192 200`
+do
+for i in `seq 1 32`
+do
+cd directory_$j
+sed "s/segment_index/$j/g" /home/white.do/DiPierroLab_Douglas/6_extra_tools/copy_trajectories_directory_N.bash > ./copy_trajectories_directory_$j.bash
+sed "s/segment_index/$j/g" /home/white.do/DiPierroLab_Douglas/6_extra_tools/submit_copy_trajectories_directory_N.bash > ./submit_copy_trajectories_directory_$j.bash
+sbatch submit_copy_trajectories_directory_$j.bash
+done
+done
--- 6_extra_tools/copy_trajectories_directory_N.bash
+++ 6_extra_tools/copy_trajectories_directory_N.bash
@@ -1,1 +1,5 @@
-cp -R ./directory_segment_index/* /work/dipierrolab/secret/final_structures/ 
+for j in `seq 1 32`
+do
+cp -R ./directory_segment_index/part_$j/traj_0.cndb /work/dipierrolab/secret/final_structures/traj_0_sim_segment_index.cndb
+cp -R ./directory_segment_index/part_$j/traj_1.cndb /work/dipierrolab/secret/final_structures/traj_1_sim_segment_index.cndb
+done
