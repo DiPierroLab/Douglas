b1a7e19bc00965fefe255c7c289610dcc3c80e09
Douglas White <white.do@northeastern.edu>
2023-11-14 17:00:26 -0500

    Rename Hi-C_adder.py as the more accurate Hi-C_averager.py

---
+++ 4_Hi-C/Hi-C_averager.py
@@ -0,0 +1,18 @@
+import numpy 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+path = "/work/dipierrolab/douglas/final_Hi-C_maps/"
+for i in {37,38,39,40}:#directory
+    HiC_Matrices = []
+    number_of_matrices = 0
+    for j in {1,2,3,4}:#part
+        number_of_matrices += 1
+        print("loading Hi-C_directory_"+str(i)+"_part_"+str(j)+".txt")
+        HiC_Matrices.append(numpy.loadtxt("//scratch//white.do//Pairing//directory_"+str(i)+"//part_"+str(j)+"//Hi-C_multi.txt"))
+    HiC_Total = sum(HiC_Matrices)/number_of_matrices
+    numpy.savetxt(path+"Hi-C_directory_"+str(i)+".txt",HiC_Total)
+    plt.matshow(HiC_Total, norm=mpl.colors.LogNorm(vmin=0.00003, vmax=1.0),cmap="Reds")
+    plt.title('directory_'+str(i))
+    plt.colorbar()
+    plt.savefig(path+"Hi-C_directory_"+str(i)+".png")
