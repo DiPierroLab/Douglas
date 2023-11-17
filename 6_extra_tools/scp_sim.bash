03753f93c6467882e2f8ff4a987f90aa0789b85e
Douglas-White <white.do@northeastern.edu>
2023-11-17 11:50:32 -0500

    Fixed scp_sim.bash paths and small bug in P_vs_d_experimental_compare_computational.ipynb

--- 6_extra_tools/scp_sim.bash
+++ 6_extra_tools/scp_sim.bash
@@ -1,7 +1,7 @@
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_41.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_42.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_43.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_44.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_41.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_42.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_43.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_44.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
 scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_41.png /Users/douglas/Desktop
 scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_42.png /Users/douglas/Desktop
 scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_43.png /Users/douglas/Desktop
