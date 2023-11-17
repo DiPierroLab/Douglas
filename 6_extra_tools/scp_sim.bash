203fd96c0f8b33bbe5ccaf2b6f7031292029ac7b
Douglas-White <white.do@northeastern.edu>
2023-11-17 09:44:34 -0500

    Made an scp file transfer bash script that transfers Hi-C images and P vs d graph data

---
+++ 6_extra_tools/scp_sim.bash
@@ -0,0 +1,8 @@
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_41.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_42.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_43.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PAAvsGenomic_directory_44.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_41.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_42.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_43.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_44.png /Users/douglas/Desktop
