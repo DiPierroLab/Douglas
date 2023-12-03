0a7c7da5b5ba443c3f91cc4a3ce554c956afa092
Douglas-White <white.do@northeastern.edu>
2023-12-03 14:58:48 -0500

    Make lambdas using NuChroM

--- 6_extra_tools/scp_sim.bash
+++ 6_extra_tools/scp_sim.bash
@@ -1,6 +1,6 @@
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_45.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_46.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_47.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_45.png /Users/douglas/Desktop
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_46.png /Users/douglas/Desktop
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_47.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_48.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_49.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_50.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_48.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_49.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_50.png /Users/douglas/Desktop
