69ef5eb04a052d31089a4b0311bcb767aca5b715
Douglas-White <white.do@northeastern.edu>
2023-12-09 16:06:41 -0500

    a

--- 6_extra_tools/scp_sim.bash
+++ 6_extra_tools/scp_sim.bash
@@ -1,8 +1,8 @@
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_51.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_52.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_53.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_54.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_51.png /Users/douglas/Desktop
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_52.png /Users/douglas/Desktop
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_53.png /Users/douglas/Desktop
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_54.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_55.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_56.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_57.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_58.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_55.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_56.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_57.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_58.png /Users/douglas/Desktop
