ab8c6a32d141e8e132a0dc58b7c03bf2a3fdfca5
Douglas-White <white.do@northeastern.edu>
2023-12-19 14:39:18 -0500

    a

--- 2_make_lambdas/plot_lambda_matrix_row.py
+++ 2_make_lambdas/plot_lambda_matrix_row.py
@@ -9,7 +9,9 @@ file = '/Users/douglas/Documents/Features Transfer/store lambdas'
 
 # Load lambdas matrices for different simulations
 print('Loading matrices')
+M41 = loadtxt(file+'/Lambdas41.txt', delimiter=',')
 M49 = loadtxt(file+'/Lambdas49.txt', delimiter=',')
+M55 = loadtxt(file+'/Lambdas55.txt', delimiter=',')
 M56 = loadtxt(file+'/Lambdas56.txt', delimiter=',')
 M57 = loadtxt(file+'/Lambdas57.txt', delimiter=',')
 M58 = loadtxt(file+'/Lambdas58.txt', delimiter=',')
@@ -29,20 +31,27 @@ print(d)
 # Make arrays for the first column of each lambdas matrix
 #     Zero out AA type interactions; this leaves only ideal chromosome interactions
 #     If you divided gamma by a number to make the lambdas matrix, then multiply back by it
-vec49 = 6.0*(M49[0:250,0]-M49[0,2499])
-vec56 = 6.0*(M56[0:250,0]-M56[0,2499])
-vec57 = 6.0*(M57[0:250,0]-M57[0,2499])
-vec58 = 6.0*(M58[0:250,0]-M58[0,2499])
-vec59 = 6.0*(M59[0:250,0]-M59[0,2499])
-vec60 = 6.0*(M60[0:250,0]-M60[0,2499])
-
+vec41 = M41[0:250,0]-M41[0,2499]
+vec49 = M49[0:250,0]-M49[0,2499]
+vec55 = M55[0:250,0]-M55[0,2499]
+vec56 = M56[0:250,0]-M56[0,2499]
+vec57 = M57[0:250,0]-M57[0,2499]
+vec58 = M58[0:250,0]-M58[0,2499]
+vec59 = M59[0:250,0]-M59[0,2499]
+vec60 = M60[0:250,0]-M60[0,2499]
 
 # Plot the lambdas slices. They should overlap.
-plot(d*bead_size_new, vec49,'-', label='sim 49: IC/6   AA/2  extrapolation first 70 beads')
-plot(d*bead_size_new, vec56,':', label='sim 56: IC/6   AA/2  extrapolation first 70 beads  0 first 40 beads')
+plot(d*bead_size_new, vec41,'-', label='sim 41 ideal chromosome / 6 first  0 first 20 beads')
+#plot(d*bead_size_new, vec49,'-', label='sim 49')#: IC/6   AA/2  extrapolation first 70 beads')
+plot(d*bead_size_new, vec55,'--', label='sim 55')#: IC/6   AA/2  extrapolation first 70 beads  0 first 40 beads')
+plot(d*bead_size_new, vec56,'--', label='sim 56')#: IC/6   AA/2  extrapolation first 70 beads  0 first 40 beads')
+plot(d*bead_size_new, vec57,'--', label='sim 57')#: IC/6   AA/2  extrapolation first 70 beads  line first 40 beads')
+plot(d*bead_size_new, vec58,'--', label='sim 58')#: IC/6   AA/2  0 first 40 beads line from 40 to 300 beads')
+plot(d*bead_size_new, vec59,'--', label='sim 59')#: IC/6   AA/2  line first 40 beads line from 40 to 300 beads')
+plot(d*bead_size_new, vec60,'--', label='sim 60')#: IC/6   AA/2  line first 40 beads line from 40 to 600 beads')
 #plot(d*250,-exp(-d/420)/12-d/300000, label = 'NuChroM gamma fit')
-axvline(x = 40*bead_size_new, color = 'black', label='genomic distance with correct contact frequency')
-#xlabel('genomic distance away from the diagonal in base pairs')
+#axvline(x = 40*bead_size_new, color = 'black', label='genomic distance with correct contact frequency')
+xlabel('genomic distance away from the diagonal in base pairs')
 #xscale('log')
 ylabel('gamma')
 #yscale('log')
