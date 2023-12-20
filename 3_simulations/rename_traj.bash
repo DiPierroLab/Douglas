5ae1656dd6ef69bf6e5fa05f20475eeecead510b
Douglas White <white.do@northeastern.edu>
2023-12-20 07:43:01 -0500

    Make run_timespan.bash and supporting files

---
+++ 3_simulations/rename_traj.bash
@@ -0,0 +1,6 @@
+mv traj_0.cndb traj_0_timespan_segment_index.cndb
+mv traj_0.ndb traj_0_timespan_segment_index.ndb
+mv traj_0.pdb traj_0_timespan_segment_index.pdb
+mv traj_1.cndb traj_1_timespan_segment_index.cndb
+mv traj_1.ndb traj_1_timespan_segment_index.ndb
+mv traj_1.pdb traj_1_timespan_segment_index.pdb
---
+++ 3_simulations/run_timespan.bash
@@ -0,0 +1,6 @@
+j=1
+cd /scratch/white.do/Pairing/directory_55/part_1
+#bash /home/white.do/DiPierroLab_Douglas/5_conversion_cndb_to_pdb/run_N_conversion.bash
+#sed "s/segment_index/$j/g" /home/white.do/DiPierroLab_Douglas/3_simulations/rename_traj.bash > rename_traj_N.bash
+#bash rename_traj_N.bash
+bash /home/white.do/DiPierroLab_Douglas/3_simulations/run_N_MiChroM_sims_from_initial_structure.bash
