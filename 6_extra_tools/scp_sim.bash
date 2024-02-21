f5b3edd32c4ee2f8cc49059762e72b40ed70eb7e
Douglas-White <white.do@northeastern.edu>
2024-02-21 18:00:47 -0500

    Organized P vs d experiment compare sim; rebinned at 5kb

--- 6_extra_tools/scp_sim.bash
+++ 6_extra_tools/scp_sim.bash
@@ -1,7 +1,3 @@
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_80.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/transPvsGenomic_directory_80.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_80.png /Users/douglas/Desktop
-
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_81.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/transPvsGenomic_directory_81.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
-scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_81.png /Users/douglas/Desktop
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/PvsGenomic_directory_59.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/PvsGenomic/transPvsGenomic_directory_59.txt '/Users/douglas/Documents/Features Transfer/store_Ps_vs_genomic_graphs'
+scp white.do@xfer.discovery.neu.edu:/work/dipierrolab/douglas/final_Hi-C_maps/Hi-C_directory_59.png /Users/douglas/Desktop
