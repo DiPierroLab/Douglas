36d263e00e0b58d06c4fc6d2384ea83774f852d5
Douglas-White <white.do@northeastern.edu>
2024-05-06 15:38:36 -0400

    A

--- 2_make_lambdas/lambda_matrix_maker_submitter.bash
+++ 2_make_lambdas/lambda_matrix_maker_submitter.bash
@@ -1,9 +1,3 @@
 
-python lambda_matrix_maker_IC_from_file.py 345 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_214.txt TTTTT -1.5 4 False True 2
-python lambda_matrix_maker_IC_from_file.py 346 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_214.txt TTTTT -1.5 5 False True 2
-python lambda_matrix_maker_IC_from_file.py 347 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_214.txt TTTTT -1.5 6 False True 2
-python lambda_matrix_maker_IC_from_file.py 348 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_214.txt TTTTT -1.5 7 False True 2
-python lambda_matrix_maker_IC_from_file.py 349 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_344.txt TTTTT -3.0 4 False True 2
-python lambda_matrix_maker_IC_from_file.py 350 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_344.txt TTTTT -3.0 5 False True 2
-python lambda_matrix_maker_IC_from_file.py 351 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_344.txt TTTTT -3.0 6 False True 2
-python lambda_matrix_maker_IC_from_file.py 352 AAAA AAAA gamma_phase_diagram_cis.txt gamma_trans_344.txt TTTTT -3.0 7 False True 2
+python lambda_matrix_maker_IC_from_file.py 361 AAAA ABAB gamma_phase_diagram_cis.txt gamma_trans_154.txt TTTTT -3.0 3 True False 2
+python lambda_matrix_maker_IC_from_file.py 362 AAAA ABAB gamma_phase_diagram_cis.txt gamma_trans_158.txt TTTTT -3.0 3 True False 2
