d03eaf920bdd68746f2f2407d8d75323a6455d6c
Douglas-White <white.do@northeastern.edu>
2024-06-13 18:35:52 -0400

    Make jupyter notebook for experiment model relationship figure

--- 2_make_lambdas/lambda_matrix_maker_submitter.bash
+++ 2_make_lambdas/lambda_matrix_maker_submitter.bash
@@ -1,12 +1,11 @@
 
-python lambda_matrix_maker.py 379 ABAB_random_offset_1 ABAB_random_offset_1 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 380 ABAB_random_offset_2 ABAB_random_offset_2 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 381 ABAB_random_offset_3 ABAB_random_offset_3 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 382 ABAB_random_offset_4 ABAB_random_offset_4 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 383 ABAB_random_offset_5 ABAB_random_offset_5 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-
-python lambda_matrix_maker.py 384 ABAB_random_offset_6 ABAB_random_offset_6 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 385 ABAB_random_offset_7 ABAB_random_offset_7 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 386 ABAB_random_offset_8 ABAB_random_offset_8 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 387 ABAB_random_offset_9 ABAB_random_offset_9 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
-python lambda_matrix_maker.py 388 ABAB_random_offset_10 ABAB_random_offset_10 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.342
+python lambda_matrix_maker.py 399 ABAB_random_offset_1 ABAB_random_offset_1 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 400 ABAB_random_offset_2 ABAB_random_offset_2 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 401 ABAB_random_offset_3 ABAB_random_offset_3 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 402 ABAB_random_offset_4 ABAB_random_offset_4 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 403 ABAB_random_offset_5 ABAB_random_offset_5 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 404 ABAB_random_offset_6 ABAB_random_offset_6 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 405 ABAB_random_offset_7 ABAB_random_offset_7 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 406 ABAB_random_offset_8 ABAB_random_offset_8 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 407 ABAB_random_offset_9 ABAB_random_offset_9 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
+python lambda_matrix_maker.py 408 ABAB_random_offset_10 ABAB_random_offset_10 TTTTT .2 2700 2 2.5 -3 3 False False -0.134 -0.25
